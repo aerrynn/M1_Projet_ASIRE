1f2ed348a32582f2faa003b85992b314833e47da
Kalessia <kalessia@live.it>
2022-03-01 16:13:15 +0100

    -

--- Alessia/prova2.py
+++ Alessia/prova2.py
@@ -16,7 +16,7 @@ from pyroborobo import SquareObject, CircleObject, MovableObject       # in OVER
 ################################################################################################################
 
 fileConfig = "config/prova2.properties" 
-nbSteps = 50
+nbSteps = 200
 
 
 
@@ -56,14 +56,16 @@ nbSteps = 50
 # To use movable objects, do not forget to set "gMovableObjects = true" in your properties file
 
 # In the configuration file, for each object, we tell that we want to use the objects that we defined.
-# Tell that we want to use an object with the id 'xxx' : physicalObjects[0].pytype = xxx 
+# Set the number of objects : gNbOfPhysicalObjects = 3
+# Tell that we want to use an object with the id 'xxx' : physicalObject[0].pytype = xxx
+# (NB. If we write physicalObjects[0].pytype = xxx, we have all the object of the same type (???) )
 # Define other features aboout the object 'xxx' : physicalObjects[0].sendMessageTo = 0
 
 # In the main, pass as parameter to Pyroborobo a dictionary mapping the pytype key to our object classes
 # object_class_dict = {'xxx' : Kale_A_Object, 'yyy' : Kale_B_Object, 'zzz' : Kale_C_Object}
 
 
-# NB. We can create a default object that doesn't need to declare their pytype. Procedure :
+# NB. We can create default objects that don't need to declare their pytype. Procedure :
 #   - in the configuration file, set existing gPhysicalObjectDefaultType to 1
 #     gPhysicalObjectDefaultType = -1
 #   - in the main, add the default object to the object_class_dict
@@ -73,119 +75,90 @@ nbSteps = 50
 
 class Kale_A_Object(CircleObject):
 
-    def __init__(self, id):
+    def __init__(self, id, data):           # put "data" even if it is not used
         CircleObject.__init__(self, id)
         self.str = "[Kale_A_Object] : "
+        self.cptSteps = 0
+        # self.rob = Pyroborobo.get()       # get pyroborobo singleton ?
 
     def reset(self):
-        print(self.str + "initialized")
-        #super().reset()
+        pass
 
     def step(self):
-        print(self.str + "step")
-        #super().step()
+        self.cptSteps += 1
+        print(self.str + "I'm object n." + str(self.id) + ", cptSteps = " + str(self.cptSteps) )
 
-    # def is_pushed(self, id, speed):
-    #     print(self.str + "is_pushed")
-    #     #super().is_pushed(id_, speed)
+    def is_pushed(self, id, speed):
+        print(self.str + "is_pushed")
 
-    # def is_touched(self, id):
-    #     print(self.str + "is_touched")
-    #     #super().is_touched(id_)
+    def is_touched(self, id):
+        print(self.str + "is_touched")
 
-    # def is_walked(self, id):
-    #     print(self.str + "is_walked")
-    #     #return super().is_walked(id_)
+    def is_walked(self, id):
+        print(self.str + "is_walked")
 
     def inspect(self, prefix=""):
-        return f"[INFO] I'm the object #{self.id}\n"
-
+        return "\n" + self.str + f"[INSPECT] I'm the object #{self.id}\n\n"
 
 #---------------------------------------------------------------------------------------------------------------
 
-""" class Kale_B_Object(SquareObject):
+class Kale_B_Object(SquareObject):
 
-    def __init__(self):
-        SquareObject.__init__(self)
-        str = "[Kale_B_Object] : "
-        print(str + "initialized")
+    def __init__(self, id, data):           # put "data" even if it is not used
+        SquareObject.__init__(self, id)
+        self.str = "[Kale_B_Object] : "
+        self.cptSteps = 0
+        self.rob = Pyroborobo.get()         # get pyroborobo singleton ?
+
+    def reset(self):
+        pass
 
     def step(self):
-        print(str + "step")
-        #super().step()
+        self.cptSteps += 1
+        print(self.str + "I'm object n." + str(self.id) + ", cptSteps = " + str(self.cptSteps) )
 
-    def is_pushed(self, id_, speed):
-        print(str + "is_pushed")
-        #super().is_pushed(id_, speed)
+    def is_pushed(self, id, speed):
+        print(self.str + "is_pushed")
 
-    def is_touched(self, id_):
-        print(str + "is_touched")
-        #super().is_touched(id_)
+    def is_touched(self, id):
+        print(self.str + "is_touched")
 
-    def is_walked(self, id_):
-        print(str + "is_walked")
-        #return super().is_walked(id_)
+    def is_walked(self, id):
+        print(self.str + "is_walked")
 
     def inspect(self, prefix=""):
-        return f"[INFO] I'm the object #{self.id}\n" """
-
+        return "\n" + self.str + f"[INSPECT] I'm the object #{self.id}\n\n"
 
 #---------------------------------------------------------------------------------------------------------------
 
-""" class Kale_C_Object(MovableObject):
+# Rappel : set "gMovableObjects = true" in your configuration file to use MovableObjects
 
-    def __init__(self):
-        MovableObject.__init__(self)
-        str = "[Kale_C_Object] : "
-        print(str + "step")
+class Kale_C_Object(MovableObject):
 
-    def step(self):
-        print(str + "step")
-        #super().step()
+    def __init__(self, id, data):           # put "data" even if it is not used
+        MovableObject.__init__(self, id)
+        self.str = "[Kale_C_Object] : "
+        self.cptSteps = 0
+        # self.rob = Pyroborobo.get()       # get pyroborobo singleton ?
 
-    def is_pushed(self, id_, speed):
-        print(str + "is_pushed")
-        #super().is_pushed(id_, speed)
-
-    def is_touched(self, id_):
-        print(str + "is_touched")
-        #super().is_touched(id_)
-
-    def is_walked(self, id_):
-        print(str + "is_walked")
-        #return super().is_walked(id_)
-
-    def inspect(self, prefix=""):
-        return f"[INFO] I'm the object #{self.id}\n" """
-
-
-#---------------------------------------------------------------------------------------------------------------
-
-""" class Kale_D_Object(CircleObject):
-
-    def __init__(self):
-        CircleObject.__init__(self)
-        str = "[Kale_D_Object] : "
-        print(str + "initialized")
+    def reset(self):
+        pass
 
     def step(self):
-        print(str + "step")
-        #super().step()
+        self.cptSteps += 1
+        print(self.str + "I'm object n." + str(self.id) + ", cptSteps = " + str(self.cptSteps) )
 
-    def is_pushed(self, id_, speed):
-        print(str + "is_pushed")
-        #super().is_pushed(id_, speed)
+    def is_pushed(self, id, speed):
+        print(self.str + "is_pushed")
 
-    def is_touched(self, id_):
-        print(str + "is_touched")
-        #super().is_touched(id_)
+    def is_touched(self, id):
+        print(self.str + "is_touched")
 
-    def is_walked(self, id_):
-        print(str + "is_walked")
-        #return super().is_walked(id_)
+    def is_walked(self, id):
+        print(self.str + "is_walked")
 
     def inspect(self, prefix=""):
-        return f"[INFO] I'm the object #{self.id}\n" """
+        return "\n" + self.str + f"[INSPECT] I'm the object #{self.id}\n\n"
 
 
 
@@ -199,18 +172,11 @@ class PythonController(Controller):
     
     def __init__(self, world_model):
         Controller.__init__(self, world_model)
-        #self.rob = Pyroborobo.get()
-        print("I'm a Python controller\n")
-        self.cptSteps = 0
 
     def reset(self):
-        print("I'm initialized\n")
+        pass
 
     def step(self):
-        self.cptSteps += 1
-        print("I'm robot n." + str(self.id) + ", cptSteps = " + str(self.cptSteps) )
-
-        #-------------------------------------------------------------------------------------------------------
 
         # Simple default definition of translation and rotation
 
@@ -226,12 +192,9 @@ class PythonController(Controller):
         elif self.get_distance_at(3) < 1 :
             self.set_rotation(-0.5)
 
-        #-------------------------------------------------------------------------------------------------------
-
 
     def inspect(self, prefix=""):
-        return f"[INFO] I'm the robot #{self.id}\n"
-
+        return f"\n[INSPECT] I'm the robot #{self.id}\n\n"
 
 
 
@@ -239,10 +202,7 @@ class PythonController(Controller):
 # MAIN
 ################################################################################################################
 
-rob = Pyroborobo.create(fileConfig, controller_class = PythonController, object_class_dict = {'xxx': Kale_A_Object})
-#                        'yyy': Kale_B_Object,              \
-#                        'zzz': Kale_C_Object,              \
-#                        '_default': Kale_D_Object})        \
+rob = Pyroborobo.create(fileConfig, controller_class = PythonController, object_class_dict = {'xxx': Kale_A_Object, 'yyy': Kale_B_Object, 'zzz': Kale_C_Object})
 
 rob.start()                                 # activation of pyroborobo simulator 
 rob.update(nbSteps)                         # activation of nbUpdates steps
