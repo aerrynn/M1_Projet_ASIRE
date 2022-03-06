aae86ac5ac979ccacaccc82fdddbda6839a2e916
Kalessia <kalessia@live.it>
2022-03-06 07:23:54 +0100

    trial

--- Alessia/trial1.py
+++ Alessia/trial1.py
@@ -27,6 +27,10 @@ cptSteps = 0                                # global, used to know the passed nu
 currentAgent = None                         # global, used to know wich agent hits one object
 tabSumFood = [0] * nbRobots                 # global, used to store the fitness function
 
+mutationRate = 0                            # global, used by HIT-EE algorithm
+transferRate = 0.5  # 0.9                   # global, used by HIT-EE algorithm
+maturationDelay = 100                       # global, used by HIT-EE algorithm
+
 verbose = True                              # set true if you want to see execution details on terminal
 
 
@@ -59,7 +63,7 @@ class Food_Object(CircleObject):
         self.unregister()
 
         global tabSumFood
-        tabSumFood[currentAgent] += 1       # foraging task
+        tabSumFood[currentAgent] += 1       # foraging task, fitness array
 
         if verbose :
             print(self.str + "is_touched by robot n." + str(currentAgent) )
@@ -81,13 +85,20 @@ class RobotsController(Controller):
     
     def __init__(self, world_model):
         Controller.__init__(self, world_model)
-        self.genome = [0] * self.nb_sensors
+
+        self.age = 0
+
+        self.genome = self.genome = [self.randNotZero() for _ in range(self.nb_sensors)]
         self.sensors = [0] * self.nb_sensors
         self.halfSizeGenome = int(np.ceil(len(self.genome)/2))
         self.halfSizeSensors = int(np.ceil(len(self.sensors)/2))
 
+        self.message = []
+        
+
     def reset(self):
         pass
+
 
     def step(self):
 
@@ -104,6 +115,9 @@ class RobotsController(Controller):
             self.sensors[i] = self.get_distance_at(i)
         print("sensors", self.sensors)
 
+        # Robots' behaviours exchange (communication)
+        self.hit_ee(mutationRate, transferRate, maturationDelay)
+
         # Expert behaviour : le robot n.0 plays the role of the expert
         if self.id == 0 :
             t, r = self.expertBehaviour()
@@ -115,6 +129,13 @@ class RobotsController(Controller):
         t, r = self.swarmBehaviour()
         self.set_translation(t)
         self.set_rotation(r)
+
+
+    def randNotZero():
+        aleaFloat = 0
+        while aleaFloat == 0:
+            aleaFloat = np.random.random()
+        return aleaFloat
 
 
     def expertBehaviour(self):
@@ -147,6 +168,20 @@ class RobotsController(Controller):
         t = max(-1, min(t, 1))
         r = max(-1, min(r, 1))
         return t, r
+
+
+    def hit_ee(self):
+        if self.age >= maturationDelay:     # The robot is ready to learn or teach
+            
+            # Teaching knowledge to 
+            self.broadcast(self.genome, transferRate, tabSumFood[self.id])
+
+            # Learning knowledge from received packets
+            for p in self.incomingPackets:
+                if p.fitness >= self.fitness:
+                    newGenome = self.transferGenome(p)
+
+
 
 
     def inspect(self, prefix=""):
